b085e38eeefb61317e18c33750ec61dfb6bc0cbc
NostalgiaJ <81918704+NostalgiaJ@users.noreply.github.com>
2021-05-12 00:00:29 +0900

    Add files via upload

---
+++ Asg_06_5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int i, j;
+
+	for (i = 1; i <= 5; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			printf("%d ", j);
+		}
+		printf("\n");
+	}
+
+	for (i = 4; i > 0; i--)
+	{
+		for (j = 0; j < i; j++)
+		{
+			printf("%d ", j);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
